b960a22a3918652d0340cf5de089eed8892dbbde
Nat Sl <slupitska@gmail.com>
2022-06-12 13:17:57 +0300

    my 7th c# prohject

---
+++ Discount/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Discount
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //приложение спрашивает пользователя, на какую сумму он сделал покупку в магазине N
+            //если сумма чека составила больше 10 евро,
+            //то пользователь получает скидку на следующую покупку в размере 1 евро
+            //если сумма чека составляет от 20 до 50 евро, то скидка на следующую покупку составляет 5 евро.
+            //если сумма чека от 50 до 100 евро, то скидка на следующую покупку - 10 евро
+            //если сумма чека превышает 100 евро, то скидка на следующую покупку - 15 евро
+
+
+            int sumShopping;
+            Console.WriteLine("To know the amount of discount on your next shopping, please enter the amount of the last check:");
+            sumShopping = Convert.ToInt32(Console.ReadLine());
+
+            if (sumShopping < 10)
+            {
+                Console.WriteLine("Sorry, discount available for purchases over 10 euro.");
+            }
+            else if (sumShopping >= 10 && sumShopping <= 20)
+            {
+                Console.WriteLine(" Your discount for next time is 1 euro.");
+            }
+            else if (sumShopping >= 20 && sumShopping <= 50)
+            {
+                Console.WriteLine("Your discount for next time is 5 euro.");
+            }
+            else if (sumShopping > 50 && sumShopping <= 100)
+            {
+                Console.WriteLine("Your discount for next time is 10 euro.");
+            }
+            else
+            {
+                Console.WriteLine("Your discount for next time is 15 euro!");
+            }
+            Console.WriteLine("Have a good shopping!");      
+        }
+    }
+}
---
+++ Rock-Paper-Scissors/Program.cs
@@ -0,0 +1,89 @@
+﻿using System;
+
+namespace Rock_Paper_Scissors
+{
+    using System;
+        class Program
+        {
+            static void Main(string[] args)
+            {
+                // ПК будет генерировать три случайных значения, от 1 до 3.
+                // 1 - камень, 2 - ножницы, 3 - бумага
+                // камень бьет ножницы, ножницы режут бумага, бумага оборачивает камень
+                // с помощью if или switch; while (break когда сумма очков = 3) или логические операторы
+                //игра должна продолжаться до того момента, пока 1 из игроком не наберет 3 очка.
+                //каждая победа в игре оценивается в 1 очко
+
+                int userScore = 0;
+                int cpuScore = 0;
+
+                Console.WriteLine("Let's play game from our childhood!");
+                Console.WriteLine("Game up to 3 points.");
+
+
+                while (userScore < 3 && cpuScore < 3)
+                {
+                    Console.WriteLine("Rock, scissors or paper?");
+                    string userChoice = Console.ReadLine().ToLower();
+
+                    Console.WriteLine($"USER Choice {userChoice}");
+
+                    Random rnd = new Random();
+                    int random = rnd.Next(1, 4);
+                    string cpuChoice = "";
+
+                    switch (random)
+                    {
+                        case 1:
+                            cpuChoice = "paper";
+                            break;
+                        case 2:
+                            cpuChoice = "rock";
+                            break;
+                        case 3:
+                            cpuChoice = "scissors";
+                            break;
+                    }
+
+                    Console.WriteLine($"CPU choice {cpuChoice}");
+
+                    if ((cpuChoice == "paper" && userChoice == "rock") || (cpuChoice == "scissors" && userChoice == "paper") || (cpuChoice == "rock" && userChoice == "scissors"))
+                    {
+                        cpuScore++;
+                        Console.WriteLine("USER lose. CPU win!");
+                    }
+                    else if ((userChoice == "rock" && cpuChoice == "scissors") || (userChoice == "paper" && cpuChoice == "rock") || (userChoice == "scissors" && cpuChoice == "paper"))
+                    {
+                        userScore++;
+                        Console.WriteLine("USER win! CPU lose.");
+                    }
+                    else if (userChoice == cpuChoice)
+                    {
+                        Console.WriteLine("Draw.");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Invalid input from USER, please try again.");
+                    }
+
+                    Console.WriteLine($"USER score: {userScore} - CPU score: {cpuScore}");
+
+                }
+
+                if (userScore == 3)
+                {
+                    userScore++;
+                    Console.WriteLine("USER is winner!");
+
+                }
+                else
+                {
+                    cpuScore++;
+                    Console.WriteLine("Cpu is winner!");
+
+                }
+
+                Console.WriteLine("Let's play again!");
+            }
+        }
+    }
