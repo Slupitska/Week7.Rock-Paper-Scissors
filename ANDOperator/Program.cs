a45c36cd71f5aa9982c6d7a0f51d3a3dc6017b72
Nat Sl <slupitska@gmail.com>
2022-06-12 23:28:04 +0300

    added ANDOperator, OROperator, gitignore

---
+++ ANDOperator/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace ANDOperator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //AND - && - строгое условие проверки каких-то выражений
+            //true && true => true - username && password => welcome!
+            //true && false => false - username && password => access denied
+            //false && true => false - username && password => access denied
+            //false && false => false -username && password => access denied
+
+            //контрольные данные: username - admin, password - "1234"
+
+            string username, password;
+            Console.WriteLine("Enter username:");
+            username = Console.ReadLine();
+
+            Console.WriteLine("Enter password:");
+            password = Console.ReadLine();
+
+            if (username == "admin" && password == "1234")
+            {
+                Console.WriteLine("Welcome!");
+            }
+            else
+            {
+                Console.WriteLine("Access denied.");
+            }
+        }
+    }
+}
